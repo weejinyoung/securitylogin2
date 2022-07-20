5bfab4291e34bc9e965544f44364a893b85f61fa
Jn W <a8118199@gmail.com>
2022-07-20 15:52:29 +0900

    Initial commit spring study security login2

---
+++ src/main/java/study/securitylogin/SecurityloginApplication.java
@@ -0,0 +1,13 @@
+package study.securitylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityloginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/securitylogin/SecurityloginApplicationTests.java
@@ -0,0 +1,13 @@
+package study.securitylogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
